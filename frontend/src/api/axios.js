ea71fe0038074c0302b71a374efa9eb22a3b1020
meenainwal <anujsinghnainwal@gmail.com>
2023-10-09 17:28:36 +0530

    update code

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 let axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api/v1",
-  baseURL: "http://13.234.238.121:5000/api/v1",
+  baseURL: "http://16.171.3.157:5000/api/v1",
 });
 export default axiosInstance;
